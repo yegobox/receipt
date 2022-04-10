52034bedc3b2709fa78c46006d1c2e71d2a75f39
Richard <beastar457@gmail.com>
2022-04-10 13:23:17 +0200

    prototyping the printing and sharing receipt

--- lib/print.dart
+++ lib/print.dart
@@ -1,81 +1,87 @@
 import 'package:pdf/widgets.dart';
 import 'package:pdf/pdf.dart';
 import 'package:receipt/omni_printer.dart';
+import 'package:flipper_models/isar_models.dart';
 
 class Print {
-  static List<TableRow> feed(List<Map<String, String>> list) {
-    List<TableRow> rows = [];
-    for (var element in list) {
+  List<TableRow> rows = [];
+  List<TableRow> feed(List<OrderItem> items) {
+    for (var item in items) {
       rows.add(TableRow(children: [
-        Text(element['quantity']!, style: const TextStyle(fontSize: 5)),
+        Text(item.count.toString()),
+        Text(item.name),
+        Text(item.price.toString()),
       ]));
     }
-    return <TableRow>[
-      TableRow(children: [
-        Text('1'),
-        Text('Coffee'),
-        Text('\$1.00'),
-      ]),
-    ];
+    return rows;
   }
 
-  static print() {
+  print() {
     OmniPrinter printer;
     printer = OmniPrinter();
-    printer.generateDoc(rows: <TableRow>[
-      TableRow(children: [
-        Text('DEC', style: TextStyle(fontWeight: FontWeight.bold)),
-        Text(
-          'ATM',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        Text('PRICE', style: TextStyle(fontWeight: FontWeight.bold)),
-      ]),
+    printer.generateDoc(
+        info: "Aurora",
+        taxId: "12331",
+        receiverName: "Richie",
+        receiverMail: "info@yegobox.com",
+        receiverPhone: "+250783054874",
+        email: "another email",
+        rows: <TableRow>[
+          TableRow(children: [
+            Text('DEC', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              'ATM',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('PRICE', style: TextStyle(fontWeight: FontWeight.bold)),
+          ]),
 
-      TableRow(children: [
-        SizedBox(height: 10),
-      ]),
+          TableRow(children: [
+            SizedBox(height: 10),
+          ]),
 
-      TableRow(children: [
-        Text('1'),
-        Text('Coffee'),
-        Text('\$1.00'),
-      ]),
+          ...rows,
+          // TableRow(children: [
+          //   Text('1'),
+          //   Text('Coffee'),
+          //   Text('\$1.00'),
+          // ]),
 
-      TableRow(children: [
-        SizedBox(height: 8),
-      ]),
+          // TableRow(children: [
+          //   SizedBox(height: 8),
+          // ]),
 
-      TableRow(children: [
-        Text('1'),
-        Text('Coffee'),
-        Text('\$1.00'),
-      ]),
+          // TableRow(children: [
+          //   Text('1'),
+          //   Text('Coffee'),
+          //   Text('\$1.00'),
+          // ]),
 
-      TableRow(children: [
-        SizedBox(height: 8),
-      ]),
+          // TableRow(children: [
+          //   SizedBox(height: 8),
+          // ]),
 
-      TableRow(children: [
-        Text('1'),
-        Text('Coffee'),
-        Text('\$1.00'),
-      ]),
+          // TableRow(children: [
+          //   Text('1'),
+          //   Text('Coffee'),
+          //   Text('\$1.00'),
+          // ]),
 
-      TableRow(children: [
-        SizedBox(height: 12),
-      ]),
+          // TableRow(children: [
+          //   SizedBox(height: 12),
+          // ]),
 
-      /// show total
-      TableRow(children: [
-        Text(
-          'TOTAL',
-          style: TextStyle(
-              color: PdfColor.fromHex('#F90006'), fontWeight: FontWeight.bold),
-        ),
-        SizedBox(),
-        Text('\$3,000', style: TextStyle(fontWeight: FontWeight.bold))
-      ])
-    ]);
+          /// show total
+          TableRow(children: [
+            Text(
+              'TOTAL',
+              style: TextStyle(
+                  color: PdfColor.fromHex('#F90006'),
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(),
+            Text('\$3,000', style: TextStyle(fontWeight: FontWeight.bold))
+          ])
+        ]);
   }
 }
