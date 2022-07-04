74d145100add6c744de97381d91e09bc2c6782e9
Richard <beastar457@gmail.com>
2022-07-04 14:31:40 +0200

    get example app to work with locator being set

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flipper_services/locator.dart';
+import 'package:get_storage/get_storage.dart';
 import 'print_example.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  // done init in mobile.//done separation.
+  setupLocator();
   runApp(const MaterialApp(home: PrintExample()));
 }
