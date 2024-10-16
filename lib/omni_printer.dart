9e2f12c667b65e59742a671877ae1a77a336c00e
Muragijimana <info@yegobox.com>
2024-10-16 13:46:56 +0200

    clean-up

--- lib/omni_printer.dart
+++ lib/omni_printer.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flipper_models/helperModels/talker.dart';
 import 'package:flipper_models/realm_model_export.dart';
 import 'package:flipper_models/view_models/mixins/riverpod_states.dart';
 import 'package:flipper_services/proxy.dart';
